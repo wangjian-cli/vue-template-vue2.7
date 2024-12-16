0ace60800f32c891755f58984a7dd5312241d6dc
wangjian27 <1223028562@qq.com>
2024-12-16 20:59:54 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,65 +1,53 @@
-import { splitVendorChunkPlugin, UserConfigExport, ConfigEnv, loadEnv } from 'vite';
+import { defineConfig, loadEnv, ConfigEnv } from 'vite';
 import vue from '@vitejs/plugin-vue2';
-import legacy from '@vitejs/plugin-legacy';
-import Components from 'unplugin-vue-components/vite';
-import AutoImport from 'unplugin-auto-import/vite';
-import { AtomResolver } from '@atom/auto-import-resolver';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import { resolve } from 'path';
-import pxtovw from 'postcss-px-to-viewport';
-import autoprefixer from 'autoprefixer';
-const loder_pxtovw = pxtovw({
-  viewportWidth: 375,
-  viewportUnit: 'vw'
-});
-// 自动添加前缀
-const loader_autoprefixer = autoprefixer({
-  overrideBrowserslist: ['Android 4.1', 'iOS 7.1'],
-  grid: true
-});
-
-export default ({ mode }: ConfigEnv): UserConfigExport => {
-  const env = loadEnv(mode, process.cwd());
+import legacy from '@vitejs/plugin-legacy';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+export default defineConfig((mode: ConfigEnv) => {
+  const env = loadEnv(mode.mode, process.cwd());
   return {
-    base: env.VITE_APP_BASE_URL,
+    base: env.VITE_MODE_BASE_URL,
+    define: {},
     plugins: [
       vue(),
-      splitVendorChunkPlugin(),
       legacy({
-        targets: ['defaults', 'Android > 44', 'Safari > 8', 'iOS > 8'],
+        // legacy不识别chrome49，所以选用chrome40
+        targets: ['chrome >= 40'],
         additionalLegacyPolyfills: ['regenerator-runtime/runtime']
       }),
-      createHtmlPlugin({
-        inject: {
-          data: {
-            ...env
-          }
-        }
-      }),
       AutoImport({
-        // 不用手动导入ref,reactive,onmounted
+        // 不用导入ref,reactive,onmounted
         imports: ['vue', 'vue-router'],
-        // 不用手动导入组件的方法
-        // resolvers:[],
+        // 不用导入element的方法
+        // resolvers: [ElementUiResolver()],
         dts: 'src/types/auto-imports.d.ts'
       }),
       Components({
-        // 不用手动导入组件
-        resolvers: [AtomResolver()],
+        // 不用导入element的组件
+        // resolvers: [ElementUiResolver()],
         dts: 'src/types/components.d.ts'
       })
     ],
+    // 路径简称
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        // _t: resolve(__dirname, 'src/utils/tools.ts'),
+        // _a: resolve(__dirname, 'src/api/index.ts'),
+        // _c: resolve(__dirname, 'src/components'),
+        vue: 'vue/dist/vue.esm.js'
+      }
+    },
     // 全局导入common.less
     css: {
       preprocessorOptions: {
         less: {
           additionalData: '@import "@/assets/style/common.less";'
         }
-      },
-      postcss: {
-        plugins: [loder_pxtovw, loader_autoprefixer]
       }
     },
+    // 本地调试起的服务
     server: {
       port: 4000,
       open: true,
@@ -75,19 +63,36 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
         }
       }
     },
+    // 编译
     build: {
-      target: 'es2015',
-      chunkSizeWarningLimit: 2000,
-      sourcemap: env.VITE_APP_CURRENTMODE !== 'release'
-    },
-    // 路径简称
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src'),
-        _t: resolve(__dirname, 'src/utils/tools.ts')
-        // _a: resolve(__dirname, 'src/api/index.ts'),
-        // _c: resolve(__dirname, 'src/components')
+      // target: 'es2015',
+      //以防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制符号的形式。 2.6.6开始有 2.6.6 打包失败 2.6.7 以上
+      cssTarget: 'chrome49',
+      //去除console
+      terserOptions: {
+        compress: {
+          // eslint-disable-next-line camelcase
+          drop_console: true,
+          // eslint-disable-next-line camelcase
+          drop_debugger: true
+        }
+      },
+      // 要去除console需要增加如下选项才能执行
+      minify: 'terser',
+      outDir: 'html',
+      assetsInlineLimit: 102400,
+      rollupOptions: {
+        output: {
+          chunkFileNames: () => 'js/[name]-[hash].js',
+          assetFileNames: (assetInfo: any) => {
+            if (/\.css$/.test(assetInfo.name)) {
+              return 'css/[name]-[hash][extname]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+          entryFileNames: 'js/[name]-[hash].js'
+        }
       }
     }
   };
-};
+});
