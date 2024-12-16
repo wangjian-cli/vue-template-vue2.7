4407e514d82279c227a73b4a7e31dcd347774182
wangjian27 <1223028562@qq.com>
2024-12-16 21:27:30 +0800

    mobile-ts

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,5 +1,4 @@
 import defaultCover from './defaultCover';
-import judgeScrollBar from './judgeScrollBar';
 import tolerant from './tolerant';
 import clickOutside from './clickOutside';
-export { defaultCover, judgeScrollBar, tolerant, clickOutside };
+export { defaultCover, tolerant, clickOutside };
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import '@/assets/style/reset.css';
 import '@/assets/style/theme/dark.css';
 import '@/assets/style/theme/light.css';
 import '@atom/b2c-tokens/dist/variables.css';
+import { defaultCover, tolerant, clickOutside } from '@/directives/index';
+
+const directives = {
+  defaultCover,
+  tolerant,
+  clickOutside
+};
+
+Object.entries(directives).forEach(([name, directives]) => {
+  Vue.directive(name, directives);
+});
 
 new Vue({
   render: (h: Vue.CreateElement) => h(App),
